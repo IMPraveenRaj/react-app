16ee7ad822a671769d745a001a3d4c7c8f59f0f2
IMPraveenRaj <impraveen13@gmail.com>
2024-03-25 17:21:53 +0100

    lets-get-classy

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user-card">
+      <h2>Name: Akshay</h2>
+      <h3>Location: Chennai</h3>
+      <h4>Contact: @impraveen13</h4>
+    </div>
+  );
+};
+
+export default User;
---
+++ src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Akshay</h2>
+        <h3>Location: Chennai</h3>
+        <h4>Contact: @impraveen13</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
