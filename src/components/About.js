e1db34680852bc93f59c0b4c1a61419a53b8c45b
IMPraveenRaj <impraveen13@gmail.com>
2024-03-26 17:12:10 +0100

    lets-get-classy

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,28 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
 
-      <h2>This is Spring Heaven</h2>
-      <User />
-      <UserClass></UserClass>
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
 
+  componentDidMount() {
+    console.log("Parent Component did Mount");
+  }
+
+  render() {
+    console.log("parent render method");
+    return (
+      <div>
+        <h1>About</h1>
+
+        <h2>This is Spring Heaven</h2>
+
+        <UserClass></UserClass>
+      </div>
+    );
+  }
+}
 export default About;
--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,14 @@
-const User = () => {
+import { useState } from "react";
+
+const User = ({ name }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
+
   return (
     <div className="user-card">
-      <h2>Name: Akshay</h2>
+      <h1>Count : {count}</h1>
+      <h1>Count : {count2}</h1>
+      <h2>Name: Akshay {name}</h2>
       <h3>Location: Chennai</h3>
       <h4>Contact: @impraveen13</h4>
     </div>
